9e87260fff13f427127a437e04798d74260ac390
Jakub Mandula <jakub.aludnam@gmail.com>
2015-03-11 07:59:57 +0100

    add data convertor

---
+++ dataconvert.py
@@ -0,0 +1,27 @@
+import re
+import csv
+
+file = open("results1.txt")
+
+data = file.read()
+lines = data.split("\n")
+finalData = []
+
+for i in lines:
+  if i[:5] == "Total":
+    continue
+  finalDataLine = []
+  fields = i.split(",")
+  for j in fields:
+    print j
+    try:
+      num = float(re.sub("[A-Za-z%()\s':]", "", j))
+    except:
+      continue
+    finalDataLine.append(num)
+
+  finalData.append(finalDataLine)
+
+with open("output.csv", "wb") as f:
+  writer = csv.writer(f)
+  writer.writerows(finalData)
