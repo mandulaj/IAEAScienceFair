63601a5f62e34c1670f27cea5112651679d2a76e
Jakub Mandula <jakub.aludnam@gmail.com>
2015-03-12 23:50:55 +0100

    add xor example

---
+++ examples/xor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import pybrain
+import math
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import TanhLayer
+from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
+
+net = buildNetwork(2, 2, 1, bias=True, hiddenclass=TanhLayer)
+
+ds = SupervisedDataSet(2, 1)
+ds.addSample((0, 0), (0,))
+ds.addSample((0, 1), (1,))
+ds.addSample((1, 0), (1,))
+ds.addSample((1, 1), (0,))
+
+trainer = BackpropTrainer(net, ds)
+
+print 'Untrained:'
+act = net.activate([1,0])[0]
+print [1,0], act, "≈", math.floor(act+0.5)
+act = net.activate([0,1])[0]
+print [0,1], act, "≈", math.floor(act+0.5)
+act = net.activate([0,0])[0]
+print [0,0], act, "≈", math.floor(act+0.5)
+act = net.activate([1,1])[0]
+print [1,1], act, "≈", math.floor(act+0.5)
+
+while trainer.train() > .001:
+    pass
+
+print 'Trained:'
+act = net.activate([1,0])[0]
+print [1,0], act, "≈", math.floor(act+0.5)
+act = net.activate([0,1])[0]
+print [0,1], act, "≈", math.floor(act+0.5)
+act = net.activate([0,0])[0]
+print [0,0], act, "≈", math.floor(act+0.5)
+act = net.activate([1,1])[0]
+print [1,1], act, "≈", math.floor(act+0.5)
